ff802f9b47c6787f4e6b805f3754e2d0ead6bf88
Mayank aneja <anejamayank16@gmail.com>
2025-03-24 02:22:48 +0530

    utils defined

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import dotenv from "dotenv"
+import express from "express"
+
+const app=express()
 
 dotenv.config({
     path:"./env"
@@ -8,3 +11,11 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server listening on port ${process.env.PORT}`)
+    } )
+})
+.catch((err)=>{
+    console.log("MONGO db connect failed")
+})
