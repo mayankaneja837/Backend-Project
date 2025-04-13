33d4d8999ceaa074f62206fd50f1510a9dbf210d
Mayank aneja <anejamayank16@gmail.com>
2025-04-13 17:36:03 +0530

    wrote some new playlist controllers

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -25,6 +25,108 @@ const createPlaylist=asyncHandler(async (req,res)=>{
     )
 })
 
+const userPlaylist=asyncHandler(async(req,res)=>{
+    const {userId}=req.params
+    if(!userId){
+        throw new ApiError(401,"User id does not exist")
+    }
+
+    const playlist=await Playlist.find({owner:userId})
+    if(!playlist){
+        throw new ApiError(401,"Playlist doesnt exist with the given UserId")
+    }
+
+    res.status(201).json(
+        new ApiResponse(201,playlist.videos,"User playlist fetched successfully")
+    )
+})
+
+const getplaylistId=asyncHandler(async(req,res)=>{
+    const {playlistId}=req.params
+    if(!playlistId){
+        throw new ApiError(401,"Playlist Id doesn't exist")
+    }
+
+    const playlist=await Playlist.findById(playlistId)
+    if(!playlist){
+        throw new ApiError(404,"Playlist with that given Id does not exist")
+    }
+
+    res.status(200).json(
+        new ApiResponse(200,playlist.videos,"Playlist with the given Id fetched successfully")
+    )
+})
+
+const addVideoToPlaylist=asyncHandler(async(req,res)=>{
+    const {playlistId}=req.params
+    const {videoIds}=req.body
+
+    if(!playlistId || !videoIds){
+        throw new ApiError(401,"Either of playlistId or VideoId or both of them doesn' exist")
+    }
+
+    const normalizedVideo=Array.isArray(videoIds) ? videoIds:[videoIds]
+    const playlist=await Playlist.findByIdAndUpdate(playlistId,{
+        $addToSet:{
+            videos:{
+                $each:normalizedVideo
+            }
+        },
+        $set:{
+            owner:req.user?._id
+        }
+    },{
+        new:true
+    })
+
+    if(!playlist){
+        throw new ApiError(400,"Error while uploading video to a playlist")
+    }
+
+    res.status(201).json(
+        new ApiResponse(201,playlist.videos,"Video added to the playlist successfully")
+    )
+})
+
+const removeVideoFromPlaylist=asyncHandler(async(req,res)=>{
+    const {playlistId,videoId}=req.params
+    if(!playlistId || !videoId){
+        throw new ApiError(401,"PlaylistId or VideoID is missing")
+    }
+
+    const playlist=await Playlist.findByIdAndUpdate(playlistId,{
+        $pull:{
+            videos:videoId
+        }
+    },{
+        new:true
+    }).select("-owner")
+
+    if(!playlist){
+        throw new ApiError(401,"Error while removing the video")
+    }
+
+    res.status(200).json(
+        new ApiResponse(200,playlist,"Video removed from the playlist successfully")
+    )
+})
+
+const deletePlaylist=asyncHandler(async(req,res)=>{
+    const {playlistId}=req.params
+    if(!playlistId){
+        throw new ApiError(401,"PlaylistId field is empty")
+    }
+    await Playlist.findByIdAndDelete(playlistId)
+
+    res.status(200).json(
+        new ApiResponse(200,{},"Playlist deleted successfully")
+    )
+})
 export {
-    createPlaylist
+    createPlaylist,
+    userPlaylist,
+    getplaylistId,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist
 }
